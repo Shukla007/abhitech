08670f65154e66e867d95988b6cb9fae4b5059ee
Abhishek Shukla <47856675+Shukla007@users.noreply.github.com>
2021-12-20 16:29:59 +0530

    Added comment in java file

--- app/src/main/java/com/smart/teach/MainActivity.java
+++ app/src/main/java/com/smart/teach/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+        // Hello World
+        System.out.printf("HEllo world");
     }
 }
