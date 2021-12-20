8b06190928877cbd458752ee769fc0916d79a4b8
Abhishek Shukla <shuklaabhishekkumar0007@gmail.com>
2021-12-20 15:23:43 +0530

    First commit

---
+++ app/src/main/java/com/smart/teach/MainActivity.java
@@ -0,0 +1,16 @@
+package com.smart.teach;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
+    }
+}
