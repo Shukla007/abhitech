3fe671c0cce6373d906bc14e6ac0fe8204713772
Abhishek Shukla <47856675+Shukla007@users.noreply.github.com>
2021-12-20 22:15:01 +0530

    New ui added

--- app/src/main/java/com/smart/teach/joinUsers_Activity.java
+++ app/src/main/java/com/smart/teach/joinUsers_Activity.java
@@ -2,6 +2,7 @@ package com.smart.teach;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -23,6 +24,8 @@ public class joinUsers_Activity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(joinUsers_Activity.this, "Student login", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(joinUsers_Activity.this,student_login.class);
+                startActivity(intent);
             }
         });
 
---
+++ app/src/main/java/com/smart/teach/teacher_login.java
@@ -0,0 +1,14 @@
+package com.smart.teach;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class teacher_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_login);
+    }
+}
