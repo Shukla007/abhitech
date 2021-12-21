0f3f7098ee5c17cb0aa5df159f23c184a958ef61
Abhishek Shukla <47856675+Shukla007@users.noreply.github.com>
2021-12-21 18:13:49 +0530

    bottom navigation added

--- app/src/main/java/com/smart/teach/MainActivity.java
+++ app/src/main/java/com/smart/teach/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,21 +13,37 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
     Button button;
-
+    BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button =(Button)findViewById(R.id.useractivity);
+        Button button = (Button) findViewById(R.id.useractivity);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,joinUsers_Activity.class);
+                Intent intent = new Intent(MainActivity.this, joinUsers_Activity.class);
                 startActivity(intent);
+
+                bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+                bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                        switch (item.getItemId())
+                        {
+                            case R.id.home_btn:
+
+                        }
+
+                        return true;
+                    }
+                });
 
             }
         });
