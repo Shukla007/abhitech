5e968e2faf6ca84b50960fda3daba9958198d50a
Shubham201676 <shubhamkumarthakur61@gmail.com>
2021-12-23 18:19:30 +0530

    Update MainActivity.java

--- app/src/main/java/com/smart/teach/MainActivity.java
+++ app/src/main/java/com/smart/teach/MainActivity.java
@@ -1,17 +1,9 @@
 package com.smart.teach;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-
 import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -21,16 +13,21 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-//import com.bumptech.glide.Glide;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import com.bumptech.glide.Glide;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
 import com.onesignal.OneSignal;
 import com.smart.teach.Fragment.Home;
 import com.smart.teach.Fragment.HomeH;
+
+//import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
     private static final String ONESIGNAL_APP_ID = "78227aaf-c889-4b2f-80e3-dd0ede27e2ff";
